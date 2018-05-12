38c51a7d2b1c2b0335a9a8694b7adddc63c6aeb7
rellbows <ryan.ellis927@gmail.com>
2018-05-12 13:02:12 -0500

    Initial skeleton setup.

---
+++ wrestler.py
@@ -0,0 +1,17 @@
+### Program Name: Wrestler
+### Filename: wrestler.py
+### Author: Ryan Ellis
+### Due Date: 5/13/18
+
+# file handling
+
+# the meat and potatoes
+
+class Wrest_Graph():
+	
+
+class Wrest_Vert():
+
+
+def main():
+	''''driver code'''
